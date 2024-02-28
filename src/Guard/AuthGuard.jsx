c357988dc69f6dc4aca087b342a37e6091e06032
Osama Yousuf <osamayousuf26@gmail.com>
2024-02-28 18:43:39 +0500

    fix

--- src/Guard/AuthGuard.jsx
+++ src/Guard/AuthGuard.jsx
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from "react-router-dom";
 const AuthGuard = () => {
     const token = localStorage?.getItem('token')
 
-    return token ? <Outlet /> : <Navigate to="/" />
+    return token ? <Outlet /> : <Navigate to="/login" />
 };
 
 export default AuthGuard;
