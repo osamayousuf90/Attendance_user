e5f47c0b4933abc4ef3e51c37de968b94e196a0d
Osama Yousuf <osamayousuf26@gmail.com>
2024-02-29 19:44:05 +0500

    fix

--- src/pages/Attendance/index.jsx
+++ src/pages/Attendance/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import moment from 'moment';
+import React, { useEffect, useState } from 'react'
 import { TfiHandPointUp } from "react-icons/tfi";
 import { useNavigate } from 'react-router-dom';
 
@@ -6,17 +7,27 @@ import { useNavigate } from 'react-router-dom';
 const Attendance = () => {
     const [checkIn, setCheckIn] = useState(false)
     const navigate = useNavigate()
+    const [time, setTime] = useState(new Date());
 
     const handleNavigate = () => {
         navigate("/history")
     }
 
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(intervalID);
+    }, []);
+
     return (
         <div className='w-full mb-5 flex flex-col items-center justify-center'>
             <div>
-                <p className='text-5xl my-4 md:my-0 md:text-2xl text-white text-center font-semibold'>05:19 PM</p>
-                <p className='text-5xl my-4 md:my-0 md:text-2xl text-white text-center font-semibold'>Wednesday</p>
-                <p className='text-5xl my-4 md:my-0 md:text-2xl text-white text-center font-semibold'>Feb 28, 2024</p>
+                <p className='text-5xl my-4 md:my-0 md:text-2xl text-white text-center font-semibold'> {moment(time).format('h:mm A')}</p>
+                <p className='text-5xl my-4 md:my-0 md:text-2xl text-white text-center font-semibold'>{moment().format('dddd')}</p>
+                <p className='text-5xl my-4 md:my-0 md:text-2xl text-white text-center font-semibold'>{moment().format('MMM D, YYYY')}</p>
             </div>
 
             <div onClick={() => setCheckIn(!checkIn)} className={`mt-14 md:mt-4 h-[200px] w-[200px] md:h-[150px] md:w-[150px] gap-3 cursor-pointer flex flex-col justify-center items-center  rounded-full ${checkIn ? "bg-[#597BDD] shadow-cyan-500/50" : "bg-[#e25a87] shadow-cyan-500/50"}`}>
